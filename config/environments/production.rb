7d23d854f21aee43d7623c2ba7acc3a1b9450f61
David Wen <dstwen@gmail.com>
2014-08-09 09:21:37 -0400

    devise setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Devise setting
+  config.action_mailer.default_url_options = { :host => 'http://zerohero.herokuapp.com' }
 end
