0b1a91e001f31a48359f5c0c17d5f2a7d48a4c9a
David Wen <dstwen@gmail.com>
2014-08-09 10:30:00 -0400

    set up Google Oauth with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # devise_for :users
   root 'pages#index'
+
+  devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks" }
 end
